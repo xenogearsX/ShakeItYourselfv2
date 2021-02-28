60017ad7085cf9f25ffd7412a943b8b3385112ca
xenogearsX <xenogears@hotmail.fr>
2021-02-28 15:12:22 +0100

    import css from static version and Route all the components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./screen/About";
+import Condition from "./screen/Condition";
+import Footer from "./components/Footer";
+import Form from "./screen/Form";
+import Header from "./components/Header";
+import Home from "./screen/Home";
+import HowTo from "./screen/HowTo";
+import Legal from "./screen/Legal";
+import Nav from "./components/Nav";
+import Politic from "./screen/Politic";
+import Recipe from "./screen/Recipe";
+import { Route, Switch } from "react-router-dom";
+
+import "./App.css";
+import RecipesList from "./screen/RecipesList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/cocktails/:id" component={Recipe} />
+        <Route path="/form" component={Form} />
+        <Route path="/about" component={About} />
+        <Route path="/howto" component={HowTo} />
+        <Route path="/legal" component={Legal} />
+        <Route path="/condition" component={Condition} />
+        <Route path="/politic" component={Politic} />
+        <Route path="/recipes" component={RecipesList} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
